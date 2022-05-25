7dee28789ed15c4e11919d05482c8670673d3ccb
Phan Duy <duyph.mangotech@gmail.com>
2022-05-25 17:02:42 +0700

    setup base

---
+++ lemo-web/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+import Header from "./layouts/Header"
+import Footer from "./layouts/Footer"
+
+function App() {
+    return (
+        <div className='App'>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
---
+++ lemo-web/src/components/Loading/index.jsx
@@ -0,0 +1,13 @@
+import "../Loading/Loading.scss"
+
+function Loading() {
+    return (
+        <div className='loading'>
+            <div className='spinner-border' role='status'>
+                <span className='visually-hidden'>Loading...</span>
+            </div>
+        </div>
+    )
+}
+
+export default Loading
